ea00cc9e1b602259cc6da0abb2db799c2c1a42e4
winterrdog <winterrdog@protonmail.ch>
2024-04-07 20:17:46 +0300

    moved all exports to one place

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+// @ts-check
+import { createPlayer, updatePlayer, getPlayer } from "./player";
+import { createSession, updateSession, getSession } from "./sessions";
+export {
+  createSession,
+  updateSession,
+  getSession,
+  createPlayer,
+  updatePlayer,
+  getPlayer,
+};
