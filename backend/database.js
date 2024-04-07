41e1d04271e8a669cf96f692e8b0b45f32abd3c9
winterrdog <winterrdog@protonmail.ch>
2024-04-07 13:37:55 +0300

    introduced player CRU operations

---
+++ backend/database.js
@@ -0,0 +1,17 @@
+// @ts-check
+import { getFirestore } from "@firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCzjy4yTM_nuqCyB9B3293ksSgSKB6xofg",
+  authDomain: "masaka-mwana.firebaseapp.com",
+  projectId: "masaka-mwana",
+  storageBucket: "masaka-mwana.appspot.com",
+  messagingSenderId: "824577596739",
+  appId: "1:824577596739:web:9d14b85d6cff62ed7ba3f6",
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
